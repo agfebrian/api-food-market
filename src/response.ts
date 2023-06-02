084bd9246a5555a1ffc1df495c9edf9779515246
agfebrian <febriananggi9@gmail.com>
2023-06-02 07:26:25 +0700

    Basic interface response

---
+++ src/response.ts
@@ -0,0 +1,10 @@
+import ResposeApi from "./types/response.interface";
+
+const responseApi: ResposeApi = {
+  status: true,
+  statusCode: 200,
+  message: "",
+  errors: [],
+};
+
+export default responseApi;
